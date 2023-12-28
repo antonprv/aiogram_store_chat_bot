6a026e888f8b8ddfc59d524acfc06103c12ea5d4
Anton <anton.prv@yandex.com>
2023-12-28 06:34:58 +0400

    Добавил код в app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardRemove
+
+from data.config import ADMINS
+from loader import dp
+
+user_message = 'Пользователь'
+admin_message = 'Админ'
+
+
+# Обработчик комманды start, показывает самое первое сообщение бота.
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message) -> None:
+    # Создаем объект-кнопки с подгонкой под размер окна телеги.
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    # Добавляем 2 кнопки в объект.
+    markup.row(user_message, admin_message)
+    
+    # Выводим сообщение и 2 кнопки.
+    await message.answer('''Привет! 👋
+
+🤖 Я бот-магазин по подаже товаров любой категории.
+
+🛍️ Чтобы перейти в каталог и выбрать приглянувшиеся 
+товары возпользуйтесь командой /shop.
+
+❓ Возникли вопросы? Не проблема! Команда /help поможет 
+связаться с админами, которые постараются как можно быстрее откликнуться.
+''', reply_markup=markup)
+
+
+# Если выбрали кнопку админа, то вносим в список, выводим сообщение,
+# кнопки выключаем.
+@dp.message_handler(text=admin_message)
+async def admin_mode(message: types.Message) -> None:
+    cid = message.chat.id
+    if cid not in ADMINS:
+        ADMINS.append(cid)
+
+    await message.answer('Включен админский режим.',
+                         reply_markup=ReplyKeyboardRemove())
+
+
+# Если юзер, то смотрим, есть ли в админах. Удаляем, если есть.
+# кнопки выключаем.
+@dp.message_handler(text=user_message) 
+async def user_mode(message: types.Message):
+    cid = message.chat.id
+    if cid in ADMINS:
+        ADMINS.remove(cid)
+
+    await message.answer('Включен пользовательский режим.',
+                         reply_markup=ReplyKeyboardRemove())
