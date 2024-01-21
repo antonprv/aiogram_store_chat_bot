d9fcdec172b57be5298e8b5521a4238fd8d6867a
Антон <anton.prv@yandex.com>
2024-01-21 07:12:43 +0400

    Почистил публичный репозиторий

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 from typing import List
 
-BOT_TOKEN: str = '6621795840:AAEMWIJAJKHWQinsGx-ajVrHP7moWebep7U'
+BOT_TOKEN: str = ''
 
 # например, ADMINS = [000000000, 1234567890]
 ADMINS: List[int] = []
